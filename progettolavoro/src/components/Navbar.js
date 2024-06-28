8ea8ea21e88379e12c95983ecaa5cbfd1a308c13
Fedo02 <federico.giacomantonio@itsrizzoli.it>
2024-06-28 09:34:29 +0200

    Create Navbar.js

---
+++ progettolavoro/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Layout, Menu, Avatar } from 'antd';
+import { MenuUnfoldOutlined, MenuFoldOutlined, UserOutlined } from '@ant-design/icons';
+
+const { Header } = Layout;
+
+const Navbar = ({ collapsed, toggleSidebar }) => {
+    return (
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger',
+                onClick: toggleSidebar,
+            })}
+            <div style={{ float: 'right', marginRight: '16px' }}>
+                <Avatar icon={<UserOutlined />} />
+            </div>
+        </Header>
+    );
+};
+
+export default Navbar;
