710583a67dc414ebe55f1b093305c1d207136fad
Th3os02 <157703336+Th3os02@users.noreply.github.com>
2024-06-28 13:06:41 +0200

    si

--- progettolavoro/src/Routes/Users.js
+++ progettolavoro/src/Routes/Users.js
@@ -3,9 +3,11 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'rec
 import supabase from '../supabaseClient';
 import { Typography, Card, Row, Col } from 'antd';
 import dayjs from 'dayjs';
+import RecentUsers from '../components/RecentUsers';
 
 const Users = () => {
     const [data, setData] = useState([]);
+    const [recentUsers, setRecentUsers] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -16,7 +18,11 @@ const Users = () => {
             if (error) {
                 console.log('Error fetching data: ', error);
             } else {
-                // Raggruppare i dati per mese
+                const monthOrder = [
+                    'January', 'February', 'March', 'April', 'May', 'June',
+                    'July', 'August', 'September', 'October', 'November', 'December'
+                ];
+
                 const groupedData = utenti.reduce((acc, utente) => {
                     const month = dayjs(utente.Data_di_registrazione).format('MMMM');
                     if (!acc[month]) {
@@ -26,13 +32,19 @@ const Users = () => {
                     return acc;
                 }, {});
 
-                // Trasformare i dati raggruppati in un array
-                const formattedData = Object.keys(groupedData).map(month => ({
-                    name: month,
-                    count: groupedData[month]
-                }));
+                // Trasformare i dati raggruppati in un array e ordinarli
+                const formattedData = Object.keys(groupedData)
+                    .map(month => ({
+                        name: month,
+                        count: groupedData[month]
+                    }))
+                    .sort((a, b) => monthOrder.indexOf(a.name) - monthOrder.indexOf(b.name));
 
                 setData(formattedData);
+
+                // Ordinare gli utenti per data di registrazione e prendere gli ultimi 5
+                const sortedUsers = utenti.sort((a, b) => dayjs(b.Data_di_registrazione) - dayjs(a.Data_di_registrazione));
+                setRecentUsers(sortedUsers.slice(0, 5));
             }
         };
 
@@ -45,7 +57,7 @@ const Users = () => {
             <Row gutter={16}>
                 <Col span={12}>
                     <Card title="Statistiche">
-                        {/* Inserisci qui le statistiche */}
+                        <RecentUsers users={recentUsers} />
                     </Card>
                 </Col>
                 <Col span={12}>
@@ -53,7 +65,7 @@ const Users = () => {
                         {data.length > 0 ? (
                             <BarChart width={500} height={300} data={data}>
                                 <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="name" />
+                                <XAxis dataKey="name" angle={-90} textAnchor="end" height={100} />
                                 <YAxis />
                                 <Tooltip />
                                 <Legend />
---
+++ progettolavoro/src/components/RecentUsers.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { List, Avatar } from 'antd';
+import dayjs from 'dayjs'; // Importa dayjs qui
+
+const RecentUsers = ({ users }) => {
+    return (
+        <List
+            itemLayout="horizontal"
+            dataSource={users}
+            renderItem={user => (
+                <List.Item>
+                    <List.Item.Meta
+                        avatar={<Avatar src="https://via.placeholder.com/40" />}
+                        title={user.email}
+                        description={dayjs(user.Data_di_registrazione).format('YYYY-MM-DD')}
+                    />
+                </List.Item>
+            )}
+        />
+    );
+};
+
+export default RecentUsers;
