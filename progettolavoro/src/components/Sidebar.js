af90ed29444389def3ed5491b8c130dc87624fa6
Th3os02 <157703336+Th3os02@users.noreply.github.com>
2024-06-28 13:45:30 +0200

    x

--- progettolavoro/src/components/Sidebar.js
+++ progettolavoro/src/components/Sidebar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const { Sider } = Layout;
 
-const Sidebar = ({ collapsed }) => (
+const Sidebar = ({ collapsed, showParking }) => (
     <Sider collapsible collapsed={collapsed}>
         <Menu theme="dark" mode="inline">
             <Menu.Item key="1">
@@ -13,9 +13,11 @@ const Sidebar = ({ collapsed }) => (
             <Menu.Item key="2">
                 <Link to="/users">Users</Link>
             </Menu.Item>
-            <Menu.Item key="3">
-                <Link to="/parking">Parking</Link>
-            </Menu.Item>
+            {showParking && (
+                <Menu.Item key="3">
+                    <Link to="/parking">Parking</Link>
+                </Menu.Item>
+            )}
         </Menu>
     </Sider>
 );
