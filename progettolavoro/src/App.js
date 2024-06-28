a43c05757b1971d416e5b98e51f99b4e54779000
Fedo02 <federico.giacomantonio@itsrizzoli.it>
2024-06-28 09:34:21 +0200

    Update App.js

--- progettolavoro/src/App.js
+++ progettolavoro/src/App.js
@@ -1,10 +1,47 @@
-function App() {
-  return (
-    <div>
-      ciao a
-    </div>
-  );
-}
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from 'antd';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Transactions from './pages/Transactions';
+import Users from './pages/Users';
+import Parking from './pages/Parking';
+
+const { Content } = Layout;
+
+const App = () => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const toggleSidebar = () => {
+        setCollapsed(!collapsed);
+    };
+
+    return (
+        <Router>
+            <Layout>
+                <Sidebar collapsed={collapsed} />
+                <Layout className="site-layout">
+                    <Navbar collapsed={collapsed} toggleSidebar={toggleSidebar} />
+                    <Content style={{ margin: '24px 16px', padding: 24, minHeight: 280 }}>
+                        <Routes>
+                            <Route
+                                path="/transactions"
+                                element={<Transactions />}
+                            />
+                            <Route
+                                path="/users" e
+                                lement={<Users />}
+                            />
+                            <Route
+                                path="/parking"
+                                element={<Parking />}
+                            />
+                        </Routes>
+                    </Content>
+                </Layout>
+            </Layout>
+        </Router>
+    );
+};
 
 export default App;
-suca
