ed0f50910a31ca9ae825157041c1d2ac8f10e1f9
Th3os02 <157703336+Th3os02@users.noreply.github.com>
2024-06-30 02:34:33 +0200

    b

--- progettolavoro/src/components/StatCards.js
+++ progettolavoro/src/components/StatCards.js
@@ -3,8 +3,7 @@ import { Card, Typography } from 'antd';
 
 const StatCard = ({ title, number, percentage }) => {
     return (
-        <Card style={{ textAlign: 'center', padding: '10px 5px', borderRadius: '8px' }}>
-            <Typography.Text>{title}</Typography.Text>
+        <Card title={title} style={{ textAlign: 'center', padding: '10px 5px', borderRadius: '8px' }}>
             <Typography.Title level={2} style={{ margin: 0 }}>{number}</Typography.Title>
             <Typography.Text type="secondary">{percentage}</Typography.Text>
         </Card>
